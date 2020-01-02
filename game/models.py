6df3d8dced12a1bdb70f0e3fb8786d87e503f2e4
arunava001OS <arunavasarkar3@gmail.com>
2020-01-02 21:50:35 +0530

    started creating the weapon shop but error in linking form data to database

--- game/models.py
+++ game/models.py
@@ -4,13 +4,24 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from multiselectfield import MultiSelectField
 
+
+WEAPONS = (('flame_thrower','Flame Thrower'),
+    ('water_jet','Water Jet'),
+    ('sledge_hammer','Sledge Hammer'),
+    ('spinning_blades','Spinning Blades'),
+    ('machine_gun','Machine Gun'),
+    ('Flipper','Flipper'),
+	('None','None')
+	)
 
 class Profile(models.Model):
 	user=models.OneToOneField(User,on_delete=models.CASCADE)
 	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 	points=models.IntegerField(default=0)
 	money=models.IntegerField(default=500)
+	weapons= MultiSelectField(choices=WEAPONS,default = 'None')
 	
 	def __str__(self):
 		return str(self.user)
--- game/urls.py
+++ game/urls.py
@@ -6,6 +6,10 @@ urlpatterns=[
 				path('signup/',views.signup,name='signup'),
 				path('login/',views.login_view,name='login'),
 				path('signout/',views.logout_view,name='logout'),
+				path('index2/',views.index2,name = 'index2'),
+				path('index2/play/',views.play,name = 'play'),
+
+
 
 			]
 			
