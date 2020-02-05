f4474d1c767d7fd65e77f305518fd435487b0d2d
arunava001OS <arunavasarkar3@gmail.com>
2020-02-06 00:00:40 +0530

    google login

--- battlebots/settings.py
+++ battlebots/settings.py
@@ -59,7 +59,7 @@ AUTHENTICATION_BACKENDS = (
  'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
  'social_core.backends.google.GoogleOpenId',  # for Google authentication
  'social_core.backends.google.GoogleOAuth2',  # for Google authentication
-#'django.contrib.auth.backends.ModelBackend',
+'django.contrib.auth.backends.ModelBackend',
 'social_core.backends.facebook.FacebookOAuth2',
 
  )
@@ -124,6 +124,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+SITE_ID =1
 
 
 
